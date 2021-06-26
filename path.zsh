b276f19a38e06814b8076f1498b5219f95916af5
krav <filipe@krav.dev>
2021-06-26 11:59:00 +0100

    ssh script fixes

--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.config/composer/vendor/bin:$PATH"
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
+# Load Node global installed binaries
+export PATH="$HOME/.local/lib:$PATH"
+
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
--- ssh.sh
+++ ssh.sh
@@ -2,6 +2,8 @@
 
 echo "Generating a new SSH key for GitHub..."
 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xclip xsel
+
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
 ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
@@ -11,10 +13,10 @@ ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 eval "$(ssh-agent -s)"
 
 touch ~/.ssh/config
-echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
 
 ssh-add -K ~/.ssh/id_ed25519
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
-echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
+echo "run 'cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard' and add it into GitHub SSH keys"
