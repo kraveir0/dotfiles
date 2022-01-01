4b5a806d40e18609f85559453467f7dd7fdaf30b
Filipe Craveiro <filipe@krav.dev>
2022-01-01 17:04:36 +0000

    docker install steps

--- install.sh
+++ install.sh
@@ -19,6 +19,14 @@ fi
 # Install requirements
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xclip xsel
 
+
+/bin/sh -c "$(curl -fsSL https://get.docker.com -o get-docker.sh)"
+/bin/sh -c "$(curl -fsSL get-docker.sh)"
+sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Update Homebrew recipes
 brew update
 
